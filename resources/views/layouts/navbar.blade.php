d05805ccd0da9e2570d899e79449aaa962eaff6e
brstreet2 <83747032+brstreet2@users.noreply.github.com>
2024-06-08 12:09:25 +0700

    feat(navbar layout): add jquery to root layout

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -15,6 +15,8 @@
         crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="{{ asset('css/home.css') }}" />
+
+    @stack('css')
 </head>
 
 <body>
@@ -244,12 +246,16 @@
         @yield('content')
     </div>
 
+    <script src="{{ asset('js/jquery-3.7.1.js') }}" />
+
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"
         integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous">
     </script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"
         integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous">
     </script>
+
+    @stack('scripts')
 </body>
 
 </html>
