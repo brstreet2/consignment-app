47e79bf9947e7a250f4b2f7d41c51ec0a0d19b63
NamaClau <92304180+NamaClau@users.noreply.github.com>
2024-06-14 20:57:29 +0700

    Debug Home Blade

--- resources/views/dashboard/home.blade.php
+++ resources/views/dashboard/home.blade.php
@@ -10,10 +10,10 @@
     <div class="container mt-4 mb-2">
         <div class="d-flex">
             <div class="nav" id="myTab" role="tablist">
-                <button type="button" id="category" class="mx-2 btn btn-transparent shadow-lg rounded-pill" data-bs-toggle="tab" data-bs-target="#top-up"  role="tab" aria-controls="top-up" aria-selected="true">
+                <button type="button" id="category" class="active  mx-2 btn btn-transparent shadow-lg rounded-pill" data-bs-toggle="tab" data-bs-target="#top-up"  role="tab" aria-controls="top-up" aria-selected="true">
                     TOP UP
                 </button>
-                <button type="button" id="category" class="mx-2 btn btn-transparent shadow-lg rounded-pill" data-bs-toggle="tab" data-bs-target="#buy-account"  role="tab" aria-controls="buy-account" aria-selected="true">
+                <button type="button" id="category" class="mx-2 btn btn-transparent shadow-lg rounded-pill" data-bs-toggle="tab" data-bs-target="#buy-account"  role="tab" aria-controls="buy-account">
                     BELI AKUN
                 </button>
             </div>
@@ -22,8 +22,8 @@
 
     <!-- Tab panes -->
     <div class="tab-content">
-        <div class="tab-pane active" id="top-up" role="tabpanel" aria-labelledby="top-up-tab" tabindex="0">@include('dashboard.top-up')</div>
-        <div class="tab-pane" id="buy-account" role="tabpanel" aria-labelledby="buy-account-tab" tabindex="0">@include('dashboard.buy-account')</div>
+        <div class="tab-pane fade active show" id="top-up" role="tabpanel" aria-labelledby="top-up-tab" tabindex="0">@include('dashboard.top-up')</div>
+        <div class="tab-pane fade" id="buy-account" role="tabpanel" aria-labelledby="buy-account-tab" tabindex="0">@include('dashboard.buy-account')</div>
     </div>
 
 </div>
