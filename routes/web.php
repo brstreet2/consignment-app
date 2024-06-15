0561a6912724bf29ca43515c933b7ce89198c280
brstreet2 <83747032+brstreet2@users.noreply.github.com>
2024-06-15 13:14:22 +0700

    feat(web routes): add group to each service or module

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,10 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/login', function(){ return view('auth.login'); })->name('login');
 
 Route::get('/', [HomeController::class, 'home'])->name('home');
 
@@ -20,3 +20,10 @@ Route::get('/top-up/name', [ProductController::class, 'index'])->name('product.i
 //     'prefix'     => '',
 // ], function () {
 // });
+
+// Auth
+Route::group(['prefix' => 'auth'], function () {
+    Route::get('/', [AuthController::class, 'index'])->name('auth.index');
+    Route::post('/register', [AuthController::class, 'register'])->name('auth.register');
+    Route::post('/login', [AuthController::class, 'login'])->name('auth.login');
+});
