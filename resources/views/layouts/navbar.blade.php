dfddf59ad45b7508231876e0e23335ba919823f4
brstreet2 <83747032+brstreet2@users.noreply.github.com>
2024-06-15 13:14:00 +0700

    fix(navbar view): fix navbar conditional check

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -9,7 +9,8 @@
     <title>Kalem Store</title>
 
     {{-- bootstrap 5.2 --}}
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
         integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
@@ -46,7 +47,9 @@
                     <div class="col-md-6">
                         <form class="submit">
                             <div class="input-group">
-                                <input type="search" id="nav-search" class=" btn btn-outline-dark py-2 text-light shadow-none text-start" placeholder="Mau top up apa hari ini?">
+                                <input type="search" id="nav-search"
+                                    class=" btn btn-outline-dark py-2 text-light shadow-none text-start"
+                                    placeholder="Mau top up apa hari ini?">
                                 <button class="btn text-secondary shadow-none" type="submit" id="search-button"
                                     style="border-start-end-radius: 13px; border-bottom-right-radius: 13px; background-color: #1e253f;">
                                     <i class="fas fa-search"></i>
@@ -57,24 +60,34 @@
 
                     <div class="col-md-6 ">
                         <div class="col-md-12 d-flex justify-content-between">
+                            @if (Auth::check())
+                                <div class="">
+                                    <a href="{{ route('auth.index') }}"
+                                        class="btn btn-danger position-relative h-100 px-4 py-2">
+                                        Logout
+                                    </a>
+                                </div>
+                            @else
+                                <div class="">
+                                    <a href="{{ route('auth.index') }}"
+                                        class="btn btn-warning position-relative h-100 px-4 py-2">
+                                        Masuk
+                                    </a>
+                                </div>
 
-                            <div class="">
-                                <a href="{{ route('login') }}" class="btn btn-warning position-relative h-100 px-4 py-2">
-                                    Masuk
-                                </a>
-                            </div>
+                                <div class="">
+                                    <a href="{{ route('auth.index') }}"
+                                        class=" btn btn-light btn-hover position-relative h-100 px-4 py-2">
+                                        Daftar
+                                    </a>
+                                </div>
 
-                            <div class="">
-                                <a href="{{ route('login') }}" class=" btn btn-light btn-hover position-relative h-100 px-4 py-2">
-                                    Daftar
-                                </a>
-                            </div>
-
-                            <div class="">
-                                <button class="btn btn-dark h-100 w-100" style="border-radius: 10px">
-                                    <i class="fa-solid fa-grip"></i>
-                                </button>
-                            </div>
+                                <div class="">
+                                    <button class="btn btn-dark h-100 w-100" style="border-radius: 10px">
+                                        <i class="fa-solid fa-grip"></i>
+                                    </button>
+                                </div>
+                            @endif
                         </div>
 
                     </div>
@@ -109,15 +122,16 @@
                         <form class="d-flex">
                             <div class="input-group">
 
-                                <input type="search" class="form-control btn btn-outline-dark py-2 text-light shadow-none text-start"
-                                placeholder="Mau top up apa hari ini?" aria-label="Search"
-                                aria-describedby="search-button"
-                                style="border-bottom-left-radius: 13px; border-start-start-radius: 13px; background-color: #1e253f">
+                                <input type="search"
+                                    class="form-control btn btn-outline-dark py-2 text-light shadow-none text-start"
+                                    placeholder="Mau top up apa hari ini?" aria-label="Search"
+                                    aria-describedby="search-button"
+                                    style="border-bottom-left-radius: 13px; border-start-start-radius: 13px; background-color: #1e253f">
 
-                            <button class="btn text-secondary shadow-none" type="submit" id="search-button"
-                                style="border-start-end-radius: 13px; border-bottom-right-radius: 13px; background-color: #1e253f;">
-                                <i class="fas fa-search"></i>
-                            </button>
+                                <button class="btn text-secondary shadow-none" type="submit" id="search-button"
+                                    style="border-start-end-radius: 13px; border-bottom-right-radius: 13px; background-color: #1e253f;">
+                                    <i class="fas fa-search"></i>
+                                </button>
 
                             </div>
                         </form>
@@ -127,14 +141,16 @@
                         <div class="col-md-12 d-flex justify-content-evenly">
 
                             <div class="">
-                                <a href="{{ route('login') }}" class=" btn btn-dark position-relative h-100 px-4 py-2"
+                                <a href="{{ route('auth.index') }}"
+                                    class=" btn btn-dark position-relative h-100 px-4 py-2"
                                     style="background-color: #ffa842; border-radius: 10px">
                                     Masuk
                                 </a>
                             </div>
 
                             <div class="">
-                                <a href="{{ route('login') }}" class=" btn btn-light btn-hover position-relative h-100 px-4 py-2"
+                                <a href="{{ route('auth.index') }}"
+                                    class=" btn btn-light btn-hover position-relative h-100 px-4 py-2"
                                     style="background-color: #FFF6E0; border-radius: 10px; color: #ffa842">
                                     Daftar
                                 </a>
@@ -180,7 +196,8 @@
                         <form class="d-flex">
                             <div class="input-group">
 
-                                <input type="search" class="form-control btn btn-outline-dark py-2 text-light shadow-none text-start"
+                                <input type="search"
+                                    class="form-control btn btn-outline-dark py-2 text-light shadow-none text-start"
                                     placeholder="Mau top up apa hari ini?" aria-label="Search"
                                     aria-describedby="search-button"
                                     style="border-bottom-left-radius: 13px; border-start-start-radius: 13px; background-color: #1e253f">
@@ -198,14 +215,16 @@
                         <div class="col-md-12 d-flex">
 
                             <div class="col-md-5 text-end">
-                                <a href="{{ route('login') }}" class=" btn btn-dark position-relative h-100 px-2 py-2"
+                                <a href="{{ route('auth.index') }}"
+                                    class=" btn btn-dark position-relative h-100 px-2 py-2"
                                     style="background-color: #ffa842; border-radius: 10px">
                                     Masuk
                                 </a>
                             </div>
 
                             <div class="col-md-5 text-center">
-                                <a href="{{ route('login') }}" class=" btn btn-light btn-hover position-relative h-100 px-2 py-2"
+                                <a href="{{ route('auth.index') }}"
+                                    class=" btn btn-light btn-hover position-relative h-100 px-2 py-2"
                                     style="background-color: #FFF6E0; border-radius: 10px; color: #ffa842">
                                     Daftar
                                 </a>
@@ -258,12 +277,18 @@
     </div>
 
     <script src="{{ asset('js/jquery-3.7.1.js') }}">
+        < script src = "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+        integrity = "sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
+        crossorigin = "anonymous" >
+    </script>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+        integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"
+        integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous">
+    </script>
 
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
-        
     @stack('scripts')
 </body>
 
