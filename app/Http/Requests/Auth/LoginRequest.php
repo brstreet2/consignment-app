03735fdc854a99ce8165e0723c50f675c7e81f7c
brstreet2 <83747032+brstreet2@users.noreply.github.com>
2024-06-15 13:12:48 +0700

    feat(login request): validated login request

---
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests\Auth;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class LoginRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'email'    => 'required|email',
+            'password' => 'required|min:6',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'email.required'        => 'Email field cannot be empty.',
+            'email.email'           => 'Please input a valid email.',
+            'password.required'     => 'Password field cannot be empty.',
+            'password.min'          => 'Password must be atleast 8 characters.'
+        ];
+    }
+}
