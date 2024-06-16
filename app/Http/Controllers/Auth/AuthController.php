c3eb7b47418864c84268f8c83cae76d609896326
brstreet2 <83747032+brstreet2@users.noreply.github.com>
2024-06-16 18:32:48 +0700

    feat(auth controller): add logout logic

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
 use App\Http\Requests\Auth\RegisterRequest;
 use App\Models\User;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
@@ -35,7 +36,7 @@ class AuthController extends Controller
 
             return back()->with("success", 'Congrats ' . $user->name . ', you have successfully created an account.');
         } catch (\Exception $e) {
-            return back()->with("error", 'Something went wrong with our server...');
+            return back()->with("error", 'Something went wrong with our server.');
             Log::error($e->getMessage());
         }
     }
@@ -48,12 +49,24 @@ class AuthController extends Controller
             $user = Auth::attempt($credentials);
             $user = Auth::user();
 
-            dd($user);
+            if (!$user) {
+                return back()->with("error", "Invalid email or password!");
+            }
             return redirect()->route('home')->with('success', 'Welcome back, ' . $user->name);
         } catch (\Exception $e) {
-            return back()->with("error", 'Something went wrong with our server...');
+            return back()->with("error", 'Something went wrong with our server.');
             Log::error($e->getMessage());
-            dd($e->getMessage());
         }
+    }
+
+    public function logout(Request $request): RedirectResponse
+    {
+        Auth::logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect()->route('home');
     }
 }
