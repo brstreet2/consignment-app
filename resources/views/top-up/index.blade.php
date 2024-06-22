ab45fc30fee90df67d98e64027add7b0ed76d4e9
NamaClau <92304180+NamaClau@users.noreply.github.com>
2024-06-22 14:42:21 +0700

    feat(reviews): top up reviews

--- resources/views/top-up/index.blade.php
+++ resources/views/top-up/index.blade.php
@@ -42,10 +42,10 @@
                             <!-- Nav tabs -->
                             <div class="d-flex">
                                 <div class="nav" id="myTab" role="tablist">
-                                    <button type="button" id="category" class="active mx-2 py-2 px-5 btn btn-transparent shadow-lg rounded-pill" data-bs-toggle="tab" data-bs-target="#detail" role="tab" aria-controls="detail" aria-selected="true">
-                                        Detail
+                                    <button type="button" id="category" class="active mx-2 py-2 px-5 btn btn-transparent shadow-lg rounded-3" data-bs-toggle="tab" data-bs-target="#detail" role="tab" aria-controls="detail" aria-selected="true">
+                                        Produk
                                     </button>
-                                    <button type="button" id="category" class="mx-2 py-2 px-5 btn btn-transparent shadow-lg rounded-pill" data-bs-toggle="tab" data-bs-target="#review" role="tab" aria-controls="review">
+                                    <button type="button" id="category" class="mx-2 py-2 px-5 btn btn-transparent shadow-lg rounded-3" data-bs-toggle="tab" data-bs-target="#review" role="tab" aria-controls="review">
                                         Ulasan
                                     </button>
                                 </div>
@@ -81,9 +81,65 @@
                                     </div>
                                 </div>
                                 <div class="tab-pane fade" id="review" role="tabpanel" aria-labelledby="review-tab" tabindex="0">
-                                    <div class=" my-4 p-2 rounded-5">
+                                    <div class=" mt-4 p-2 rounded-5">
                                         <h5 class="card-title mb-4">Ulasan Produk</h5>
-                                        <div>asd</div>
+                                        <div class="row">
+                                            <div class="col-4 d-flex justify-content-between">
+                                                <div class="card rounded-4">
+                                                    <p class="h1 text-yellow p-3 my-1">4.9</p>
+                                                </div>
+                                                <div class="align-items-center my-auto">
+                                                    <div id="stars">
+                                                        <i class="bi bi-star-fill text-yellow fs-5"></i>
+                                                        <i class="bi bi-star-fill text-yellow fs-5"></i>
+                                                        <i class="bi bi-star-fill text-yellow fs-5"></i>
+                                                        <i class="bi bi-star-fill text-yellow fs-5"></i>
+                                                        <i class="bi bi-star-half text-yellow fs-5"></i>
+                                                    </div>
+                                                    <div>
+                                                        <small>Dari 5 Reviews</small>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="col-4">
+
+                                            </div>
+                                            <div class="col-4">
+
+                                            </div>
+                                        </div>
+                                        @for ($i = 0; $i < 5; $i++)
+                                            <hr>
+                                            <div class="row">
+                                                <div class="col-1 my-auto">
+                                                    <img src="{{ asset('images/navbar/logo.png') }}" width="56px" alt="">
+                                                </div>
+                                                <div class="col-9 d-flex justify-content-between">
+                                                    <div class="col-7">
+                                                        <p class="fw-bold">Kal** St*** <br><small class="fw-normal">{{ date('d M Y, H:i:s', strtotime(now())) }}</small></p>
+                                                    </div>
+                                                    <div class="col-5 text-center my-auto">
+                                                        <p>100 <img src="{{ asset('images/logo-cash/diamonds-mole.png') }}" width="25px"></p>
+                                                    </div>
+                                                </div>
+                                                <div class="col-2">
+                                                    <i class="bi bi-star-fill text-yellow fs-6"></i>
+                                                    <i class="bi bi-star-fill text-yellow fs-6"></i>
+                                                    <i class="bi bi-star-fill text-yellow fs-6"></i>
+                                                    <i class="bi bi-star-fill text-yellow fs-6"></i>
+                                                    @if ($i == 0)
+                                                    <i class="bi bi-star-half text-yellow fs-6"></i>
+                                                    @else
+                                                    <i class="bi bi-star-fill text-yellow fs-6"></i>
+                                                    @endif
+                                                </div>
+                                            </div>
+                                        @endfor
+                                        <div class="mt-3 text-center">
+                                            <button class="btn btn-outline-light py-2 px-4">
+                                                Tampilkan Semua
+                                            </button>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
