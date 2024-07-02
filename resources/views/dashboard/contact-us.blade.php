68e247706507c5182d43750612e1a89968f24d23
NamaClau <92304180+NamaClau@users.noreply.github.com>
2024-07-03 00:12:25 +0700

    fix(contact-us)

--- resources/views/dashboard/contact-us.blade.php
+++ resources/views/dashboard/contact-us.blade.php
@@ -13,10 +13,10 @@
             </div>
 
             <div class="col-md-6">
-                <p class="h2">Customer Services</p>
+                <p class="h2">Customer Service</p>
 
                 <a href="{{ route('faq') }}" class="text-decoration-none text-light">
-                    <div class="card bg-dark-secondary p-2 my-auto">
+                    <div class="rounded-3 bg-dark-third p-2 my-auto">
                         <div class="d-flex">
                             <div class="col-md-2">
                                 <img src="{{ asset('images/faq/faq-search-glass.png') }}" class="w-75">
@@ -32,8 +32,8 @@
                         </div>
                     </div>
                 </a>
-                <a href="{{ route('faq') }}" class="text-decoration-none text-light m-1">
-                    <div class="card bg-dark-secondary p-3">
+                <a href="discord://" target="_blank" class="text-decoration-none text-light m-1">
+                    <div class="rounded-3 bg-dark-third p-3">
                         <div class="d-flex">
                             <div class="col-md-2">
                                 <img src="{{ asset('images/email.png') }}" class="w-75">
@@ -49,8 +49,10 @@
                         </div>
                     </div>
                 </a>
-                <a href="{{ route('faq') }}" class="text-decoration-none text-light m-1">
-                    <div class="card bg-dark-secondary p-3">
+
+                {{-- nomor didapat dari IG KalemStore --}}
+                <a href="https://wa.me/+6289659305978?text=Halo, Saya Butuh Bantuan" target="_blank" class="text-decoration-none text-light m-1">
+                    <div class="rounded-3 bg-dark-third p-3">
                         <div class="d-flex">
                             <div class="col-md-2">
                                 <img src="{{ asset('images/whatsapp-logo.png') }}" class="w-75">
@@ -70,33 +72,46 @@
         </div>
 
         {{-- tengah --}}
-        <div class="bg-dark-secondary p-3 rounded-3">
+        <div class="bg-dark-third p-3 rounded-3">
             <div class="d-flex">
-                <div class="col-md-6">
-                    <p class="text-center my-auto h5">Terjadi kendala dengan transaksi kamu?</p>
-                    <p class="text-center my-auto fs-6">Jangan sungkan kirim laporan ke kami</p>
+                <div class="col-md-6 my-auto">
+                    <p class="text-center my-auto h3 fw-bold">Terjadi kendala dengan transaksi kamu?</p>
+                    <p class="text-center my-auto fs-5">Jangan sungkan kirim laporan ke kami</p>
                 </div>
                 <div class="col-md-6 bg-black p-3 rounded-3">
-                    <div class="">
-                        <p class="h4">Formulir Laporan</p>
 
-                        <label class="mt-3" for="#nama">Nama</label>
-                        <input type="text" class="btn bg-white rounded-3 col-12 py-2 text-start text-dark fs-5" name="nama" id="nama" placeholder="Masukkan Nama Kamu Disini..">
+                    {{-- methodnya nanti ganti ke POST --}}
+                    <form action="#form" method="GET" id="form">
+                        @csrf
+
+                        <p class="h4">Formulir Laporan</p>
+                        <p class="fs-6 text-muted">Silahkan Buat Laporan Kamu di Formulir Berikut</p>
+
+                        <label class="mt-2" for="#report-category">Tipe Laporan</label>
+
+                        <select class="btn bg-white rounded-3 col-12 py-2 text-start text-dark fs-6" name="report-category">
+                            <option selected disabled hidden>Silahkan Pilih Kategori Laporanmu</option>
+                            <option value="Masukan">Masukan / Feedback</option>
+                            <option value="Kendala Akun">Kendala Akun</option>
+                            <option value="Kendala Transaksi">Kendala Transaksi</option>
+                        </select>
+
+                        <label class="mt-2" for="#nama">Nama</label>
+                        <input type="text" class="btn bg-white rounded-3 col-12 py-2 text-start text-dark fs-6" name="nama" id="nama" placeholder="Masukkan Nama Kamu Disini..">
                         
                         <label class="mt-3" for="#phone">No. Whatsapp</label>
-                        <input type="text" class="btn bg-white rounded-3 col-12 py-2 text-start text-dark fs-5" name="phone" id="phone" placeholder="Masukkan Nomor WA Kamu Disini..">
+                        <input type="text" class="btn bg-white rounded-3 col-12 py-2 text-start text-dark fs-6" name="phone" id="phone" placeholder="Masukkan Nomor WA Kamu Disini..">
                         
                         <label class="mt-3" for="#question">Pertanyaan</label>
-                        <textarea name="question" class="btn bg-white rounded-3 col-12 py-2 text-start text-dark fs-5"></textarea>
+                        <textarea name="question" class="btn bg-white rounded-3 col-12 py-2 text-start text-dark fs-6" placeholder="Tulis Pertanyaanmu Disini.."></textarea>
                         
                         {{-- <button class="col-md-12 bg-yellow mt-3 py-3 rounded-4 fs-5 fw-bold text-light">SUBMIT</button> --}}
 
-                        <button class="submitBtn">
-                            Submit
-                            <svg fill="white" viewBox="0 0 448 512" height="1em" class="arrow"><path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"></path></svg>
-                          </button>
+                        <div class="text-end my-3">
+                            <button type="submit" class="submitBtn col-md-4 fs-5">Kirim</button>
+                        </div>
 
-                    </div>
+                    </form>
                 </div>
             </div>
         </div>
